6c27644c846bf9a5183b6f27a1f65a0970406cfa
Javier Jaramago Fernandez <jaramago.fernandez.javier@gmail.com>
2019-07-16 06:41:23 -0400

    NTCK: Started working into function to launch the simulation in child process

---
+++ navigation_test.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import rospy
+import subprocess
+import signal
+
+# Launch a new simulation
+
+s_params = {
+    "cur_speed": [0.2, 0.4, 0.7, 1.2, 1.4],
+    "ra": [10, 20, 30, 40, 50],
+    "tug_speed": [1.0, 3.0, 4.0, 5.0, 7.0],
+    "obs_speed": [0, 2.0, 3.0, 4.0, 6.0]
+}
+
+"""
+Function to launch a new simulation with the specified parameters.
+
+:param params: Parameters to change before starting the simulation. The parameters should be
+a dictionary with the shape:
+
+params = {
+    "cur_speed": [],
+    "ra": [],
+    "tug_speed": [],
+    "obs_speed": []
+}
+
+The arrays should have the same number of elements.
+"""
+def launch_simulation(params):
+    child = subprocess.Popen(["roslaunch","collision_avoidance_in_singapore","two_obstacles_head.launch"])
+    #child.wait() #You can use this line to block the parent process untill the child process finished.
+    # Printing process information
+    print("parent process")
+    print(child.poll())
+
+    rospy.loginfo('The PID of child: %d', child.pid)
+    print ("The PID of child:", child.pid)
+
+rospy.sleep(30)
+
+child.send_signal(signal.SIGINT) #You may also use .terminate() method
+#child.terminate()
+
+#for more: https://docs.python.org/2/library/subprocess.html
